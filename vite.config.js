920009bee4db9c7a141b6b36b85b4c927d7a012c
fbiluoficial <fekobilu@gmail.com>
2025-01-01 20:08:18 -0300

    Configurando GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/shop-list-2/',
   server: {
     port: 5173,
     strictPort: true,
