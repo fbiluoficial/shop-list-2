c0cd0b4149272545c1f3aba131de89170467b448
fbiluoficial <fekobilu@gmail.com>
2025-01-01 20:14:45 -0300

    Atualizando configuração do Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/shop-list-2/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   server: {
     port: 5173,
     strictPort: true,
